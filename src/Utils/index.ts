e8c5fd4f21c8c752c0e0ee0cad72cdc8cef0758d
Alexander Joaquin Cardozo <a.joaquincardozo@gmail.com>
2022-12-27 11:35:47 -0300

    Se emprolija navegacion de iframe

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,4 +1,9 @@
-export const getCookie = (name: string) => {
+export type NavigateInInframeConfig = {
+    baseUrl: string;
+    route: string;
+}
+
+const getCookie = (name: string) => {
     const cookieByName: Record<string, string> = document
         .cookie
         .split(";")
@@ -8,4 +13,19 @@ export const getCookie = (name: string) => {
         }, {});
 
     return cookieByName[name];
+}
+
+export const setCookieInIframe = (iframe: HTMLIFrameElement, route: NavigateInInframeConfig["route"]) =>
+    iframe.contentWindow?.postMessage(JSON.stringify({
+      cookie: {
+        name: "auth-login",
+        value: getCookie("auth-login"),
+      },
+      route
+    }), "*");
+
+export const navigateInIframe = (iframe: HTMLIFrameElement, config: NavigateInInframeConfig) => {
+    setCookieInIframe(iframe, config.route);
+    const { baseUrl, route } = config;
+    iframe.src = `${baseUrl}${route}`;
 }
