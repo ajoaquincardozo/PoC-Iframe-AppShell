aa88f515a5313c37ce36fba068243b1ce18d377b
Alexander Joaquin Cardozo <a.joaquincardozo@gmail.com>
2022-12-27 13:32:39 -0300

    Se vuelve a descomentar la autenticacion

--- src/Components/auth.ts
+++ src/Components/auth.ts
@@ -1,20 +1,20 @@
 const API_SERVER = "https://poc-module-federation-app-server.vercel.app"//"http://localhost:8081"//"https://poc-module-federation-app-server.vercel.app"
 
-// export const authenticate = async (username: string, password: string) =>
-//     fetch(`${API_SERVER}/auth/login`, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({ username, password,}),
-//     })
-//     .then((res) => {
-//         console.log(res);
-//         return res.json();
-//     })
-
-
 export const authenticate = async (username: string, password: string) =>
-    Promise.resolve({
-        access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hcmlhIiwic3ViIjoyLCJpYXQiOjE2NzIxNDQwMDcsImV4cCI6MTY3MjIzMDQwN30.JhcgwpWVSBgHBZfDVUec2llhYdMejguauCT1ksEvJuY"
+    fetch(`${API_SERVER}/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password,}),
     })
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+
+
+// export const authenticate = async (username: string, password: string) =>
+//     Promise.resolve({
+//         access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hcmlhIiwic3ViIjoyLCJpYXQiOjE2NzIxNDQwMDcsImV4cCI6MTY3MjIzMDQwN30.JhcgwpWVSBgHBZfDVUec2llhYdMejguauCT1ksEvJuY"
+//     })
