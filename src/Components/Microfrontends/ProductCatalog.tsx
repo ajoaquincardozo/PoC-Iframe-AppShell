37c0820460936f8c6601e8d1f536395b8c2b6db8
Alexander Joaquin Cardozo <a.joaquincardozo@gmail.com>
2022-12-27 12:54:15 -0300

    Se agrega comunicacion hijo a padre

--- src/Components/Microfrontends/ProductCatalog.tsx
+++ src/Components/Microfrontends/ProductCatalog.tsx
@@ -1,15 +1,28 @@
 import { useEffect, useRef } from "react";
 import { navigateInIframe } from "../../Utils";
+import { AllowedDomains, AllowedDomainType } from "../Routes";
 
 export default function ProductCatalog() {
   const iframeRef = useRef<HTMLIFrameElement>(null);
-  const baseUrl = "http://localhost:8080"
+  const baseUrl: AllowedDomainType = AllowedDomainType.development;
+
+  const handleMessageIFrame = (event: MessageEvent) => {
+    if (AllowedDomains.includes(event.origin) && event.source === iframeRef.current?.contentWindow)
+      alert(JSON.stringify(event.data, null, 2));
+  }
 
   useEffect(() => {
     navigateInIframe(iframeRef.current as HTMLIFrameElement, {
       baseUrl,
       route: "/products"
     })
+
+    window.addEventListener('message', handleMessageIFrame, false);
+
+    //Clean up
+    return () => {
+      window.removeEventListener("message", handleMessageIFrame, false);
+    }
   }, [])
 
   return (<iframe className="w-full h-full" ref={iframeRef} src={baseUrl}/>);
--- src/Components/Routes/index.ts
+++ src/Components/Routes/index.ts
@@ -1,1 +1,11 @@
 export { default as GlobalRoutes } from "./GlobalRoutes";
+
+export enum AllowedDomainType {
+    development = "http://localhost:8080",
+    production = "https://poc-iframe-products.vercel.app"
+}
+
+export const AllowedDomains = [
+    "http://localhost:8080",
+    "https://poc-iframe-products.vercel.app"
+]
